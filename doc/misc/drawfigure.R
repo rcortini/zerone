3995f0c6bfc363319281edcf0e089317ac93e1a5
gui11aume <guillaume.filion@gmail.com>
2016-01-23 21:05:50 +0100

    Colorized Figure 1.

--- doc/misc/drawfigure.R
+++ doc/misc/drawfigure.R
@@ -9,17 +9,17 @@ ynb = table(rnbinom(size=1.983, prob=0.421, nrow(dat)))
 yzinb = table(c(rep(0, (1-0.880)*nrow(dat)),
           rnbinom(size=3.842, prob=0.554, 0.880*nrow(dat))))
 
-pdf("figure.pdf", width=6, height=6)
+pdf("~/Dropbox/figure.pdf", width=6, height=6)
 plot(0:20, y[1:21] / sum(y), type="n", ylim=c(0,.25),
     xlab="Read count (300 bp windows)", ylab="Frequency")
-rect(xleft=0, xright=20, ybottom=0, ytop=.25, border=NA, col="grey96")
+rect(xleft=0, xright=20, ybottom=0, ytop=.25, border=NA, col="grey98")
 segments(x0=rep(0,4), y0=c(.05,.1,.15,.2),
     x1=rep(20,4), lwd=2, col="white")
 lines(0:20, y[1:21] / sum(y), type="h", lwd=3)
-lines(0:20+.15, ypois[1:21] / sum(y), type="h", lwd=3, col="grey75")
-lines(0:20+.3, ynb[1:21] / sum(y), type="h", lwd=3, col="grey50")
-lines(0:20+.45, yzinb[1:21] / sum(y), type="h", lwd=3, col="grey25")
+lines(0:20+.15, ypois[1:21] / sum(y), type="h", lwd=3, col="red")
+lines(0:20+.3, ynb[1:21] / sum(y), type="h", lwd=3, col="blue")
+lines(0:20+.45, yzinb[1:21] / sum(y), type="h", lwd=3, col="seagreen3")
 legend(legend=c("Observed", "Poisson", "NB", "ZINB"),
     x="topright", inset=0.04, lwd=3,
-    col=c("black", "grey75", "grey50", "grey25"), bg="white")
+    col=c("black", "red", "blue", "seagreen3"), bg="white")
 dev.off()
