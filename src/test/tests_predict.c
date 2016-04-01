aa8a3495981e7e788e36465db4ba3ed3f5453a12
nanakiksc <polcusco@gmail.com>
2016-04-01 13:07:58 +0200

    Modified SVM.

--- src/test/tests_predict.c
+++ src/test/tests_predict.c
@@ -31,20 +31,20 @@ test_predict
       0.4816441, 0.3493823, 0.8808826, 6.960893e-03
    };
 
-   test_assert(fabs(predict(zscale(case1)) - 1.00014284) < 1e-3);
+   test_assert(fabs(predict(zscale(case1)) - 0.1941607268227056870558) < 1e-4);
 
    double case2[8] = {
       0.006321302, 0.006369056, 2.620211e-26, 0.9321181,
       4.2291074, 6.2477326, 0.9995843, 4.922989e-05
    };
 
-   test_assert(fabs(predict(zscale(case2)) + 1.00024599) < 1e-3);
+   test_assert(fabs(predict(zscale(case2)) + 0.805839273176933046372) < 1e-4);
 
    double case3[8] = {
       0.043084589, 0.478401608, 1.193816e-04, 0.7304060,
       0.5678939, 1.4512983, 0.9649980, 2.195285e-03
    };
 
-   test_assert(fabs(predict(zscale(case3)) - 0.06892158) < 1e-3);
+   test_assert(fabs(predict(zscale(case3)) + 0.3861388408513803938149) < 1e-4);
 
 }
